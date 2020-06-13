b34621ab32d8d80ed347cd40e42feea34a5645b5
asusrid <32960104+asusrid@users.noreply.github.com>
2020-06-13 17:52:04 -0500

    mine_block and genesis methods set to statics and add to class Block

--- block.py
+++ block.py
@@ -1,22 +1,6 @@
 
 import time
 
-def mine_block(last_block, data):
-	"""
-	Mine a block on the given last_block and data.
-	"""
-	timestamp = time.time_ns()
-	last_hash = last_block.hash
-	hash = f'{timestamp}-{last_hash}'
-
-	return Block(timestamp, last_hash, hash, data)
-
-def genesis():
-	"""
-	Generate the genesis block
-	"""
-	return Block(1, 'genesis_last_hash', 'genesis_hash', [])
-
 class Block:
 
 	"""
@@ -38,9 +22,28 @@ class Block:
 			f'Data: {self.data})'
 		)
 
+	@staticmethod
+	def mine_block(last_block, data):
+		"""
+		Mine a block on the given last_block and data.
+		"""
+		timestamp = time.time_ns()
+		last_hash = last_block.hash
+		hash = f'{timestamp}-{last_hash}'
+
+		return Block(timestamp, last_hash, hash, data)
+
+	@staticmethod
+	def genesis():
+		"""
+		Generate the genesis block
+		"""
+		return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
+
 def main():
-	genesis_block = genesis()
-	block = mine_block(genesis_block, 'foo')
+	genesis_block = Block.genesis()
+	block = Block.mine_block(genesis_block, 'foo')
 	print(block)
 
 if __name__ == '__main__':
