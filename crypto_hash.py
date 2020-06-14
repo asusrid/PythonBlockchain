3aedec70c0741b475ba2671561e9ab78f423865b
asusrid <32960104+asusrid@users.noreply.github.com>
2020-06-14 17:55:19 -0500

    Hash function implemented for the blockchain.

---
+++ crypto_hash.py
@@ -0,0 +1,20 @@
+
+import hashlib
+import json
+
+def crypto_hash(*args):
+	"""
+	Return a sha-256 hash of the given arguments
+	"""
+	stringified_args = sorted(map(lambda data: json.dumps(data), args))
+	joined_data = ''.join(stringified_args)
+
+	return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
+
+def main():
+	print(f"crypto_hash(1, 'one', [1]): {crypto_hash(1, 'one', [1])}")
+	print(f"crypto_hash('one', 1, [1]): {crypto_hash('one', 1, [1])}")
+
+
+if __name__ == '__main__':
+	main()
