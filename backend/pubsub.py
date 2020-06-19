a9309a909c29d8062678b60da3987e2397a1537d
asusrid <32960104+asusrid@users.noreply.github.com>
2020-06-19 11:36:47 -0500

    Reformat of PubSub

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -7,23 +7,36 @@ pnconfig = PNConfiguration()
 pnconfig.subscribe_key = 'sub-c-9b74736c-b244-11ea-afa6-debb908608d9'
 pnconfig.publish_key = 'pub-c-559c320d-2803-499d-90ae-0630c2040afb'
 
-pubnub = PubNub(pnconfig)
 
 TEST_CHANNEL = 'TEST_CHANNEL'
-
-pubnub.subscribe().channels([TEST_CHANNEL]).execute()
 
 
 class Listener(SubscribeCallback):
 	def message(self, pubnub, message_object):
-		print(f'\n-- Incoming message_object: {message_object}')
+		print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
 
 
-pubnub.add_listener(Listener())
+class PubSub():
+	"""
+	Handles the pub/sub layer of the app
+	Provides communication between the nodes of the blockain net
+	"""
+	def __init__(self):
+		self.pubnub = PubNub(pnconfig)
+		self.pubnub.subscribe().channels([TEST_CHANNEL]).execute()
+		self.pubnub.add_listener(Listener())
+
+	def publish(self, channel, message):
+		"""
+		Publish the message object to the channel
+		"""
+		self.pubnub.publish().channel(channel).message(message).sync()
 
 
 def main():
-	pubnub.publish().channel(TEST_CHANNEL).message({'foo':'bar'}).sync()
+	pubsub = PubSub()
+	pubsub.publish(TEST_CHANNEL, {'foo':'bar'})
+	
 
 if __name__ == '__main__':
 	main()
